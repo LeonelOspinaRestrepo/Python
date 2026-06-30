cd806037f62ce720bc67924be66901f0b077f88a
Leonel Ospina Restrepo <leonelospinare@gmail.com>
2026-06-30 04:39:02 +0200

    Add files via upload

---
+++ codigo_fuente/tipos_de_datos.py
@@ -0,0 +1,7 @@
+edad = 20
+nombre = "Carlos"
+esEstudiante = True
+
+print ("La variable edad es", edad)
+print ("La variable nombre es-. ",nombre)
+print ("La variable es estudiante es ",esEstudiante)
